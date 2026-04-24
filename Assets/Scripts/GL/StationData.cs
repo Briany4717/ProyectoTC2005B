626c3f15e6704f1f959832de88fb2b8f12112bea
SrPika <104371533+RichGN28@users.noreply.github.com>
2026-04-24 13:27:10 -0600

    Limpieza de archivos duplicados y corrección de rutas post-merge

--- Assets/Scripts/GL/StationData.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-// clase para ligar la estacion con su icono
-[CreateAssetMenu(fileName = "NewStation", menuName = "Overcooked/Station")]
-public class StationData : ScriptableObject
-{
-    public string stationName;
-    public Sprite stationIcon;
-}
