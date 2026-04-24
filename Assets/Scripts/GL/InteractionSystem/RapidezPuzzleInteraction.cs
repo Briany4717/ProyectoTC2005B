088c56617913b8d31f38130f989a7c0bf77b17a4
SrPika <104371533+RichGN28@users.noreply.github.com>
2026-04-24 13:30:48 -0600

    Ordenes ya funcionales junto con sus estacione (#12)
    * Sistema de ordenes
    
    Listo el sistema de ordenes. Ya se genera aleatoriamente las ordenes con estaciones aleatorias, falta limitarlo a 5.
    
    * Seleccion de ordenes y funcion para completar estaciones listo
    
    Ya puedes seleccionar las ordenes las cuales quieres completar su acción, todavia hay un pequeño bug porque seleccionas en el lado contrario pero es lo de menos.
    
    * Limpieza de archivos duplicados y corrección de rutas post-merge

--- Assets/Scripts/GL/InteractionSystem/RapidezPuzzleInteraction.cs
+++ Assets/Scripts/GL/InteractionSystem/RapidezPuzzleInteraction.cs
@@ -1,8 +1,7 @@
 using UnityEngine;
 
-public class QuestionPuzzleInteraction : MonoBehaviour, IInteractable
+public class RapidezPuzzleInteraction : MonoBehaviour, IInteractable
 {
-
     public StationData station;
     public void Interact()
     {
