7d6b4885e8e2230b78f21bdfd0abd4393d52dec5
SrPika <104371533+RichGN28@users.noreply.github.com>
2026-04-27 21:35:37 -0600

    GL DragDrop
    El drag and drop ya funciona correctamente, ya se. borran las ordenes solamente cuando la orden esta completa lo borra y te da los puntos

---
+++ Assets/Scripts/GL/GLDragDrop.cs
@@ -0,0 +1,76 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GLDragDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler
+{
+    // [SerializeField] private Canvas canvas;
+    private RectTransform rectTransform;
+    private CanvasGroup canvasGroup;
+
+    // elementos para regresar la orden si no se completa
+    private Vector2 initialAnchoredPosition;
+    private Transform initialParent;
+
+    private bool wasDroppedOnValidZone = false;
+
+
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        canvasGroup = GetComponent<CanvasGroup>();
+
+
+
+
+    }
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+
+        Debug.Log("OnBeginDrag!!");
+        canvasGroup.alpha = 0.6f;
+        canvasGroup.blocksRaycasts = false;
+
+
+        initialAnchoredPosition = rectTransform.anchoredPosition;
+        initialParent = transform.parent;
+
+    }
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnDrag!!");
+        // esto mueve el mouse la posicion anterior del mouse
+        rectTransform.anchoredPosition += eventData.delta;
+
+    }
+    public void OnEndDrag(PointerEventData eventData)
+    {
+
+        Debug.Log("OnEndDrag!!");
+        canvasGroup.alpha = 1f;
+        canvasGroup.blocksRaycasts = true;
+
+        if (!wasDroppedOnValidZone)
+        {
+            ReturnToStart();
+        }
+    }
+    // se llama cuando apretamos el boton encima del objeto
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("Order pressed!!");
+    }
+    public void ReturnToStart()
+    {
+        transform.SetParent(initialParent, false);
+        rectTransform.anchoredPosition = initialAnchoredPosition;
+    }
+
+    public void MarkAsDropped()
+    {
+        wasDroppedOnValidZone = true;
+    }
+
+
+}
