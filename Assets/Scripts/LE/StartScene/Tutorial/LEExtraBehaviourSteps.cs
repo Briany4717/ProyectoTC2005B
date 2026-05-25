de439c823888dfc6f67b62dcc91b80b1a2b33530
Brianyan4717 <132787816+Brianyan4717@users.noreply.github.com>
2026-05-24 22:04:13 -0600

    Generate play button

--- Assets/Scripts/LE/StartScene/Tutorial/LEExtraBehaviourSteps.cs
+++ Assets/Scripts/LE/StartScene/Tutorial/LEExtraBehaviourSteps.cs
@@ -4,9 +4,15 @@ public class LEExtraBehaviourSteps : MonoBehaviour
 {
     public GameObject gellyBox;
     public GameObject playMenu;
+    public GameObject playBtn;
     public void Step2ExtraB()
     {
         gellyBox.SetActive(false);
         playMenu.SetActive(true);
+    }
+
+    public void FinalStepExtraB()
+    {
+        playBtn.SetActive(true);
     }
 }
